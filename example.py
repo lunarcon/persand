91aaa1d5d376ce03971aa61840ca93e5a055808e
Aditya Vikram <adityavikram2016@gmail.com>
2023-04-22 23:50:58 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 import persand.persand as p
 p.run(__file__)
-print("Hello, world! I'm a Sand Simulation!")
+print("This code is actually executed, isnt that cool?")
 
 #                  ***
 #                 *****
@@ -15,4 +15,5 @@ print("Hello, world! I'm a Sand Simulation!")
 
 # lorem ipsum dolor sit amet, consectetur adipiscing elit.
 # sed non risus. suspendisse lectus tortor.
-# cras elementum ultrices diam. maecenas ligula massa.             
+# cras elementum ultrices diam. maecenas ligula massa.
+print("This code is executed too, since the program is loaded in memory.")
