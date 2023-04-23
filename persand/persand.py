9c2871369ef930678bc1eb9157efc6f77561e4c2
Aditya Vikram <adityavikram2016@gmail.com>
2023-04-22 23:51:31 -0400

    Update persand.py

--- persand/persand.py
+++ persand/persand.py
@@ -1,24 +1,27 @@
 from time import sleep as sp; SPEED=1
 def run(file):
     has_future = [[1]]
+    with open(file, 'r+') as f:
+        lines = f.readlines()
+        lines = [i[:-1]+' '*(max([len(i) for i in lines])-len(i)) for i in lines]
+        f.seek(0)
+        f.write('\n'.join(lines))
     while sum([sum(i) for i in has_future])>0:
         with open(file, 'r+') as f:
-            lines = f.readlines()
-            max_len = max([len(i)-1 for i in lines])
-            lines = [str(i)[:-1]+' '*(max_len-len(i)-1) for i in lines]
-            has_future = [[0 for i in range(len(lines[y]))] for y in range(len(lines)-1)]
-            for y in range(len(lines)-1):
+            lines = [i[:-1] for i in f.readlines()]
+            has_future = [[0 for i in range(len(lines[y]))] for y in range(len(lines))]
+            for y in range(len(lines)):
                 for x in range(len(lines[y])):
                     c = lines[y][x]
-                    if y+1<len(lines)-1:
+                    if y+1<len(lines) and x<len(lines[y+1]):
                         c_below = lines[y+1][x]
                     else:
                         c_below = ''
-                    if x-1>=0 and y+1<len(lines)-1:
+                    if x-1>=0 and y+1<len(lines):
                         c_below_left = lines[y+1][x-1]
                     else:
                         c_below_left = ''
-                    if x+1<len(lines[y+1]) and y+1<len(lines):
+                    if y+1<len(lines) and x+1<len(lines[y+1]):
                         c_below_right = lines[y+1][x+1]
                     else:
                         c_below_right = ''
@@ -34,11 +37,5 @@ def run(file):
                             lines[y+1] = lines[y+1][:x+1]+c+lines[y+1][x+2:]
                             lines[y] = lines[y][:x]+' '+lines[y][x+1:]   
                     f.seek(0)
-                    for i in lines:
-                        f.write(i+'\n')
+                    f.write('\n'.join(lines))
         sp(0.1/SPEED)
-
-
-
-
-        
